b48cd900a48a3479252b1173b75f7f68006dec4c
Shamil <105405530+Shaamiilll@users.noreply.github.com>
2024-03-13 11:06:50 +0530

    async-actions

---
+++ asyncActions.js
@@ -0,0 +1,56 @@
+const redux = require('redux')
+const CreateStore = redux.createStore
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: "",
+};
+
+const FETCH_USER_REQUESTED = "FETCH_USER_REQUESTED";
+const FETCH_USER_SUCCEEDED = "FETCH_USER_SUCCEEDED";
+const FETCH_USER_FAILED = "FETCH_USER_FAILED";
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USER_REQUESTED,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USER_SUCCEEDED,
+    payload: users,
+  };
+};
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USER_FAILED,
+    payload: error,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USER_REQUESTED:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USER_SUCCEEDED:
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case FETCH_USER_FAILED:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+  }
+};
+
+const store = CreateStore(reducer)
